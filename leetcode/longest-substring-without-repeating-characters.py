8a5926a214852918b6474f1396dc12a0af213269
jennifer shi <jennifershi255@gmail.com>
2026-01-02 13:47:39 -0500

    Update solution for longest-substring-without-repeating-characters

--- leetcode/longest-substring-without-repeating-characters.py
+++ leetcode/longest-substring-without-repeating-characters.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Remember for DYNAMIC SLIDING WINDOW problems to set right pointer to increment in the for loop
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         
-        dups = set()
         left = 0
         longest = 0
-        
+        dups = set()
+
         for right in range(len(s)):
 
             while s[right] in dups:
                 dups.remove(s[left])
                 left += 1
-
+            
             dups.add(s[right])
-            longest = max(longest, right - left + 1)
-            right += 1
-
+            length = right - left + 1
+            longest = max(longest, length)
+        
         return longest
