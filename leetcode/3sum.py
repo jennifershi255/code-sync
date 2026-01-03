b541866f71dc4616027817b0a4112da9b90b644a
jennifer shi <jennifershi255@gmail.com>
2026-01-03 12:03:16 -0500

    Update solution for 3sum

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -8,28 +8,26 @@ class Solution:
 
             if nums[i] > 0:
                 break
-            
-            if i > 0 and nums[i] == nums[i-1]:
+
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-                
+            
             left = i + 1
             right = len(nums) - 1
 
             while left < right:
-                
-                three = nums[i] + nums[left] + nums[right]
 
-                if three > 0:
+                if nums[i] + nums[left] + nums[right] > 0:
                     right -= 1
-                elif three < 0:
+                elif nums[i] + nums[left] + nums[right] < 0:
                     left += 1
                 else:
                     res.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-
                     while nums[left] == nums[left - 1] and left < right:
                         left += 1
-            
-        
+                    while nums[right] == nums[right + 1] and left < right:
+                        right -= 1
+  
         return res
