06d2341bd6b860a408740a7a85dbef553e50337d
jennifer shi <jennifershi255@gmail.com>
2026-01-04 15:15:39 -0500

    Update solution for valid-parentheses

--- leetcode/valid-parentheses.py
+++ leetcode/valid-parentheses.py
@@ -4,24 +4,12 @@ class Solution:
         stack = deque()
         par = {"(": ")", "{": "}", "[":"]"}
 
-        if len(s) % 2 != 0:
-            return False
-
         for c in s:
-            
             if c in par:
                 stack.append(par[c])
-                continue
 
-            if stack:
-                popped = stack.pop()
-                
-                if (popped != str(c)):
+            else:
+                if not stack or stack.pop() != str(c):
                     return False
-            elif c not in par:
-                return False
-        
-        if not stack:
-            return True
-        else:
-            return False
+
+        return not stack
