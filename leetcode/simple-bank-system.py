f3138947ab4d8fdcf701bfc9ec36cf4d8b433154
jennifer shi <jennifershi255@gmail.com>
2026-01-13 21:45:33 -0500

    Add solution for simple-bank-system

---
+++ leetcode/simple-bank-system.py
@@ -0,0 +1,30 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.length = len(balance) - 1
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if (account1 - 1 > self.length) or (account2 - 1 > self.length):
+            return False
+
+        return self.withdraw(account1, money) and self.deposit(account2, money)
+
+    def deposit(self, account: int, money: int) -> bool:
+        if (account - 1) <= self.length:
+            self.balance[account - 1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if ((account - 1) <= self.length and self.balance[account - 1] >= money):
+            self.balance[account - 1] -= money
+            return True
+        return False
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
