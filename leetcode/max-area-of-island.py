2ea4590e5ddc6227960b0b219570e695fa5561d1
jennifer shi <jennifershi255@gmail.com>
2026-05-18 22:50:59 -0400

    Update solution for max-area-of-island

--- leetcode/max-area-of-island.py
+++ leetcode/max-area-of-island.py
@@ -1,21 +1,30 @@
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        res = 0
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
 
-        def dfs(r,c):
-            if r >= 0 and r < rows and c >= 0 and c < cols and grid[r][c] == 1:
+        if not grid:
+            return 0
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        res = 0
+        
+        def dfs(r,c,total):
+
+            if r >= 0 and r < ROWS and c >= 0 and c < COLS and grid[r][c] == 1:
                 grid[r][c] = 0
-                return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1)
+                return 1 + dfs(r-1,c,total) + dfs(r+1,c,total) + dfs(r,c-1,total) + dfs(r,c+1,total) 
+            
             else:
                 return 0
-
-        for r in range(rows):
-            for c in range(cols):
+        
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == 1:
-                    area = dfs(r,c)
-                    res = max(res, area)
+                    area = dfs(r,c,0)
+                    res = max(area,res)
         
         return res
